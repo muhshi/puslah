e056c45022745dccea658198a30bb522943c1900
muhshi <amuhshi@gmail.com>
2025-09-08 07:37:29 +0700

    masukin dashboar ke panel provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Dashboard;
 use App\Filament\Pages\ProfileMe;
 use App\Http\Middleware\EnsureProfileComplete;
 use Filament\Http\Middleware\Authenticate;
@@ -35,7 +36,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-
+                Dashboard::class,
+                ProfileMe::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
