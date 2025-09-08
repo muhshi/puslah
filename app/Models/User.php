d601f1eca88817d5d7fbebd04e5387b3435fa2d3
muhshi <amuhshi@gmail.com>
2025-09-08 08:33:07 +0700

    nambahin panel provider

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -69,5 +71,11 @@ class User extends Authenticatable
             ]);
         });
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
 
 }
