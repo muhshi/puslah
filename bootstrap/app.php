d216cb027c0cac16b94b112d27f57f4de8c4366f
muhshi <amuhshi@gmail.com>
2025-09-08 08:43:27 +0700

    nambahin panel provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,15 +12,24 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            at: '*', // percaya semua proxy (Cloudflare, dst)
-            headers: Request::HEADER_X_FORWARDED_FOR
+    ->withMiddleware(function ($middleware) {
+        // 1) Trusted Proxies
+        $proxies = env('TRUSTED_PROXIES', '*');
+        $headers = Request::HEADER_X_FORWARDED_FOR
             | Request::HEADER_X_FORWARDED_HOST
             | Request::HEADER_X_FORWARDED_PORT
             | Request::HEADER_X_FORWARDED_PROTO
-            | Request::HEADER_X_FORWARDED_AWS_ELB,
-        );
+            | Request::HEADER_X_FORWARDED_AWS_ELB;
+
+        $middleware->trustProxies($proxies, $headers);
+
+        // 2) (Opsional tapi disarankan) Trusted Hosts
+        $hosts = array_filter(array_map('trim', explode(',', (string) env('TRUSTED_HOSTS', 'puslah.bpsdemak.com'))));
+        if (!empty($hosts)) {
+            $middleware->trustHosts($hosts);
+        }
+
+        // middleware lainmu yang sudah ada…
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
