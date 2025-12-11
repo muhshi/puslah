1fde424438f97c84ef22dedb21bc46a94cfcde2b
muhshi <muhshi>
2025-12-11 16:32:56 +0700

    tambah navigation group

--- app/Filament/Resources/SuratTugasResource.php
+++ app/Filament/Resources/SuratTugasResource.php
@@ -25,7 +25,10 @@ class SuratTugasResource extends Resource
 {
     protected static ?string $model = SuratTugas::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationGroup = 'Manajemen Dokumen';
+    protected static ?string $navigationLabel = 'Surat Tugas';
+
 
     public static function form(Form $form): Form
     {
