e815546ded7fb7eeccd7477bba4abb4892d98eb4
muhshi <amuhshi@gmail.com>
2026-01-13 07:39:44 +0700

    ubah nama app

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
                 'danger' => Color::Red,
             ])
             ->font('Inter')
-            ->brandName('PUSLAH')
+            ->brandName('DINAMIT')
             ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
