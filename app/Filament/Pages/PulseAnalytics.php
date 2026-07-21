6ddbe378b896bcfa8f7d7a90cd29250a6e740998
muhshi <muhshi>
2026-07-21 07:47:44 +0700

    feat: add PulseAnalytics filament page to redirect to external dashboard

--- app/Filament/Pages/PulseAnalytics.php
+++ app/Filament/Pages/PulseAnalytics.php
@@ -2,15 +2,17 @@
 
 namespace App\Filament\Pages;
 
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 
 class PulseAnalytics extends Page
 {
+    use HasPageShield;
     protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-line';
     protected static ?string $navigationGroup = 'Pengaturan';
     protected static ?int $navigationSort = 99;
     protected static ?string $title = 'Pulse Analytics';
-    
+
     // Gunakan view kosong karena kita akan langsung redirect
     protected static string $view = 'filament.pages.pulse-analytics';
 
