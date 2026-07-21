60f603ea9e966762a09051b05e2a92e63cc5da6c
muhshi <muhshi>
2026-07-21 07:54:54 +0700

    fix: Use iframe for Pulse dashboard to avoid Livewire DOM issues

--- resources/views/filament/pages/pulse-analytics.blade.php
+++ resources/views/filament/pages/pulse-analytics.blade.php
@@ -1,32 +1,5 @@
 <x-filament-panels::page>
-    <div class="pulse-embed">
-        <style>
-            .pulse-embed {
-                --pulse-bg: transparent;
-            }
-            .pulse-embed .pulse-card {
-                border-radius: 0.75rem;
-            }
-        </style>
-
-        <x-pulse>
-            <livewire:pulse.servers cols="full" />
-
-            <livewire:pulse.usage cols="4" rows="2" />
-
-            <livewire:pulse.queues cols="4" />
-
-            <livewire:pulse.cache cols="4" />
-
-            <livewire:pulse.slow-queries cols="8" />
-
-            <livewire:pulse.exceptions cols="6" />
-
-            <livewire:pulse.slow-requests cols="6" />
-
-            <livewire:pulse.slow-jobs cols="6" />
-
-            <livewire:pulse.slow-outgoing-requests cols="6" />
-        </x-pulse>
+    <div class="w-full" style="height: 85vh;">
+        <iframe src="{{ url('/pulse') }}" class="w-full h-full border-0 rounded-xl bg-gray-50 dark:bg-gray-950" allowfullscreen></iframe>
     </div>
 </x-filament-panels::page>
